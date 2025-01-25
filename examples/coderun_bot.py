059c83d0a8685f1486e1c44d22743699b0a514f5
nanguage <nanguage@yahoo.com>
2025-01-24 18:09:26 -0800

    updates

--- examples/coderun_bot.py
+++ examples/coderun_bot.py
@@ -9,8 +9,12 @@ from pantheon.remote import run_toolsets
 
 async def main():
     toolset = PythonInterpreterToolSet("python_interpreter")
-    async with run_toolsets([toolset], log_level="INFO"):
-        agent = Agent("coderun_bot", "You are an AI assistant that can run Python code.")
+    async with run_toolsets([toolset], log_level="WARNING"):
+        agent = Agent(
+            "coderun_bot",
+            "You are an AI assistant that can run Python code.",
+            model="gpt-4o",
+        )
         await agent.remote_toolset(toolset.service_id)
         await agent.chat()
 
